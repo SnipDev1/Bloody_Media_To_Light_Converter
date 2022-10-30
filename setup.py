da806feab29398c469d2697774297a97ec86dff4
SnipDev1 <111853324+SnipDev1@users.noreply.github.com>
2022-10-30 16:07:17 +0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,7 @@
+import pip
+
+pip.main(["install", "art", "--user"])
+pip.main(["install", "opencv-python", "--user"])
+pip.main(["install", "webcolors", "--user"])
+pip.main(["install", "getch", "--user"])
+pip.main(["install", "moviepy", "--user"])
